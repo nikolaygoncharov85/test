945bb7cdab8a4552e3226ca479399c6408e43632
Web <web@inflottravel.com>
2020-01-30 17:59:00 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
     console.log('ready');
+    $(".search_filters input, .search_filters select").styler();
+    $(".input_begin_date input").datepicker({
+        dateFormat: 'dd.mm.yy',
+        minDate: new Date( 2016, 4, 1 ),
+        autoclose: false,
+        beforeShow: function () {
+
+        },
+        onSelect: function() {
+
+        },
+        onClose: function() {
+
+        }
+    });
     $(".selected_country").click(function(){
         if($(".left_sidebar .country_list").hasClass('country_list_showed')){
             $(".left_sidebar .country_list").removeClass('country_list_showed');
